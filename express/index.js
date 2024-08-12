357900ef42397f3d76e5b6e60ecc4a902e4bd29d
Inaldo Monteiro <inaldomonteiroti@gmail.com>
2024-08-12 17:00:47 -0300

    guiadeperguntas

--- express/index.js
+++ express/index.js
@@ -3,15 +3,32 @@ const app = express(); // carrrega iniciando todo o express nesssa variável app
 
 //Rotas caminhos para a aplicação
 app.get("/", function(req, res){
-    res.send("Seja bem vindo !"); // Só pode enviar uma resposta
+    res.send("<h1>Seja bem vindos !<?h1>"); // Só pode enviar uma resposta
 });
 
-app.get("/blog", function(req, res){
-    res.send("Seja bem vindo ao meu blog !"); // Só pode enviar uma resposta
+app.get("/blog/:artigo?", function(req, res){
+   
+var artigo = req.params.artigo; 
+    if(artigo){
+        res.send("Seja bem vindo ao meu blog !" + artigo); 
+    }else{
+        res.send("Seja bem vindo ao meu blog !"); // Só pode enviar uma resposta
+    }
+
 });
 
 app.get("/canal/youtube", function(req, res){
-    res.send("Seja bem vindo ao meu canal !"); // Só pode enviar uma resposta
+
+    var canal = req.query["canal"] // queryparametros // estão entrando em desuso
+    res.send("Seja bem vindo ao meu canal ! >>>> " + canal); // Só pode enviar uma resposta
+});
+
+app.get("/ola/:nome/:empresa", function(req, res){
+    //Req : dados enviados do ususario
+    //Res : resposta para o ussuario
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send ("Oi " + nome + empresa ); // Só pode enviar uma resposta
 });
 
 app.listen(3000, function(error){
